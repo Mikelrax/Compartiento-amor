26c37b82de482fe9840acda37a3a0c6ce6c6ce35
Daniel <danny2003renato@gmail.com>
2023-11-29 21:16:18 -0500

    v0.02

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image'
+'use client';
+
+import Slider from './ui/Components/Slider';
+import Navbar from './ui/navbar/navbar'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <p></p>
-    </main>
+    <>
+      <Navbar></Navbar>    
+      <Slider></Slider>
+    </>
+
   )
 }
